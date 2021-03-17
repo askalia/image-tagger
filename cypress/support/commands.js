8911bd462be2c7a61f8a42673cab21e5852b6127
joris grouillet <joris@askalia.net>
2021-03-17 13:56:46 +0100

    cleanup commented code

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -20,60 +20,6 @@ Cypress.Commands.add(
     },
 );
 
-Cypress.Commands.add(
-  'attach_json',
-{
-  prevSubject: 'element',
-},
-(input, fileName, fileType) => {
-    /*cy.fixture(fileName)
-      .then((content) => {
-        return new File([content], fileName);
-      })
-      .then(cy.log)
-      .then((testFile) => {
-        
-        //const testFile = new File([blob], fileName);
-        const dataTransfer = new DataTransfer();
-
-        dataTransfer.items.add(testFile);
-        input[0].files = dataTransfer.files;        
-        return input;
-      });
-  },*/  
-  /*cy.fixture(fileName).then(fileContent => {
-    input.upload({ fileContent, fileName, mimeType: 'application/json' });
-  });
-*/
-});
-
-/*Cypress.Commands.add(
-  'attach_json',
-  {
-    prevSubject: 'element',
-  },
-(input, fileName) => {
-    
-      
-    cy.fixture(fileName)
-      .then((content) => {
-        const json = JSON.stringify(content);
-        cy.log("json : " + json)
-        const file = new File([content], fileName);
-        cy.log(file)
-        const list = new DataTransfer()
-      
-        list.items.add(file)
-        const myFileList = list.files
-      
-        input[0].files = myFileList
-        return input
-        
-      })
-      
-});
-*/
-
 Cypress.Commands.add('file_to_data_uri', (fileName, fileType) => {
   return cy.fixture(fileName)
   .then((content) => {
